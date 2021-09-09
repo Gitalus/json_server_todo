fd9f3aba3203a78299c347ac9bc5c65067c9e205
Italo Guajardo <italog.penaloza@gmail.com>
2021-09-09 10:55:52 -0300

    create todo using API from academy

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -4,7 +4,7 @@ import { useForm } from './hooks/useForm';
 export const TodoApp = () => {
 
     const [todos, setTodos] = useState([]);
-    const [url] = useState("http://localhost:3001/todos")
+    const [url] = useState('https://assets.breatheco.de/apis/fake/todos/user/gitalus')
 
     const [{ description }, handleInputChange, reset] = useForm({
         description: ''
@@ -22,74 +22,122 @@ export const TodoApp = () => {
     };
 
     const getData = () => {
-        fetch(url)
-            .then(resp => resp.json())
+        fetch(url, {
+            'Content-Type': 'application/json',
+            PARAMS: 'None'
+        })
+            .then(resp => {
+                if (resp.status === 404) {
+                    createList();
+                    throw new Error("Usuario no encontrado, se creará uno nuevo");
+                } else {
+                    return resp.json()
+                }
+            })
             .then(data => {
-                setTodos(data)
+                setTodos(data);
             })
             .catch(err => console.log(err));
     }
 
-
-    const postData = () => {
+    const createList = () => {
         fetch(url, {
             method: 'POST',
             headers: {
                 'Content-type': 'application/json'
             },
-            body: JSON.stringify({
-                description
-            })
-        })
-            .then(({ ok }) => ok && getData())
-            .catch(err => console.log(err));
-    };
-
-    const handleDelete = (id) => {
-        fetch(`${url}/${id}`, {
-            method: 'DELETE'
+            body: "[]"
         })
             .then(({ ok }) => ok && getData())
             .catch(err => console.log(err));
     }
 
+    const postData = () => {
+
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify([
+                ...todos,
+                {
+                    label: description,
+                    done: false
+                }])
+        })
+            .then(({ ok }) => ok && getData())
+            .catch(err => console.log(err));
+    };
+
+    const handleDelete = (index) => {
+
+        const _todos = todos.filter((todo, _index) => _index !== index);
+
+        if (_todos.length === 0) {
+            fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+            })
+                .then(({ ok }) => {
+                    if (ok) {
+                        setTodos([]);
+                        getData();
+                    }
+                })
+                .catch(err => console.log(err));
+        } else {
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(_todos)
+            })
+                .then(({ ok }) => ok && getData())
+                .catch(err => console.log(err));
+        }
+    };
+
     return (
         <>
             <h1>todos</h1>
-            <div className="page">
+            <div className='page'>
                 <form onSubmit={handleSubmit}>
                     <input
-                        className="item-list"
-                        type="text"
+                        className='item-list'
+                        type='text'
                         value={description}
                         onChange={handleInputChange}
-                        name="description"
-                        autoComplete="off"
-                        placeholder="Create a todo item..."
+                        name='description'
+                        autoComplete='off'
+                        placeholder='Create a todo item...'
                     />
                     <hr />
                 </form>
                 {
-                    todos.map(todo => (
+                    todos.map(({ label }, index) => (
                         <div
-                            className="item-container"
-                            key={todo.id}
+                            className='item-container'
+                            key={index}
                         >
                             <div
-                                className="item-list"
+                                className='item-list'
                             >
-                                {todo.description}
+                                {label}
                             </div>
                             <i
-                                className="fas fa-times"
-                                onClick={() => handleDelete(todo.id)}></i>
-                            <hr className="w100" />
+                                className='fas fa-times'
+                                onClick={() => handleDelete(index)}></i>
+                            <hr className='w100' />
                         </div>
                     ))
                 }
                 {
-                    todos.length > 0 ? <div className="item-list footer">{todos.length} Item{todos.length === 1 ? "" : "s"} left</div>
-                        : <div className="item-list footer">No tasks, add a task</div>
+                    todos.length > 0 ? <div className='item-list footer'>{todos.length} Item{todos.length === 1 ? '' : 's'} left</div>
+                        : <div className='item-list footer'>No tasks, add a task</div>
                 }
                 <hr />
             </div>
